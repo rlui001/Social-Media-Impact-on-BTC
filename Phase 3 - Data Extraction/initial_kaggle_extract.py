4eb480e0510f9a177e5a39229836776358aa1460
rlui001 <rlui001@ucr.edu>
2022-01-04 17:35:25 -0800

    add second dataset and store strings in vars

--- Phase 3 - Data Extraction/initial_kaggle_extract.py
+++ Phase 3 - Data Extraction/initial_kaggle_extract.py
@@ -4,18 +4,22 @@ import logging
 #set logger level 
 logging.basicConfig(level=logging.INFO)
 
-#test api connection
-api = kaggle.api
-print (api.get_config_value('username'))
+#local path to save data
+localdir = '/Users/ron/Desktop/Springboard/Kaggle Data/'
+
+# ---- kaggle datasets to download ----
+#bitcoin tweets dataset
+btc_tweets = 'alaix14/Bitcoin-tweets-20160101-to-20190329'
+#bitcoin price data
+btc_price = 'mczielinski/bitcoin-historical-data'
 
 ## Code to download dataset
 logging.info('Begin downloading dataset...\n')
 
 try:
-    kaggle.api.dataset_download_files('alaix14/Bitcoin-tweets-20160101-to-20190329', \
-        path='/Users/ron/Desktop/Springboard/Kaggle Data/',\
-             unzip=True)
+    kaggle.api.dataset_download_files(btc_price, path=localdir, unzip=True)
     logging.info('Download and unzip complete\n')
 
 except Exception as e:
-    logging.critical(f'{e}')
+    logging.critical(f'Failed due to: {e}')
+
