1c67779f05586748662042d395181fb06587160b
rlui001 <rlui001@ucr.edu>
2022-01-08 05:01:07 -0800

    coinbase api initial setup

--- Phase 3 - Data Extraction/initial_cb_extract.py
+++ Phase 3 - Data Extraction/initial_cb_extract.py
@@ -1,17 +1,23 @@
 import requests
-
+import csv
+import pandas as pd
+# TO-DO: Convert into a function that takes parameters to replace below
+# TO-DO: Set up iterator to do several requests 
 pair = 'BTC-USD'
-granularity = '900'
+granularity = '300'
 start = '2021-06-01'
-end = '2021-06-02'
+end = '2021-06-022'
 
 url = "https://api.exchange.coinbase.com/products/" + \
     pair + "/candles?granularity=" + \
-    granularity + "&" + start + "&" + end
+    granularity + "&start=" + start + "&end=" + end
 
+response = requests.get(url).json()
 
-headers = {"Accept": "application/json"}
+df = pd.DataFrame(response, columns=['time', 'low', 'high', 'open', 'close', 'volume'])
 
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
+if df.empty:
+    print ('Failed to retrieve Coinbase data.\n')
+else:
+    print (df.head())
+    df.to_csv('results_cb.csv')
